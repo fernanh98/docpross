ca64d2513ed6d47edbdbc9124319b5e648b4c998
fernanh98 <fernanh98@gmail.com>
2026-06-22 17:47:24 +0200

    remove docling stuff

--- docling/__init__.py
+++
@@ -1,5 +0,0 @@
-"""docling-lightonocr – Docling OCR plugin for LightOnOCR-2-1B-bbox-soup."""
-
-from docling_lightonocr.ocr_model import LightOnOcrModel, LightOnOcrOptions
-
-__all__ = ["LightOnOcrModel", "LightOnOcrOptions"]
